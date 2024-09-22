8d027665d1cba736d98d2d6fc8393a9ee5ffef1f
kleantmenkulazi <kleantmenkulazi@gmail.com>
2024-09-22 23:55:09 +0200

    Aggiunto card info

--- src/components/AppCards.vue
+++ src/components/AppCards.vue
@@ -1,22 +1,32 @@
 <script>
+import { store } from '../store.js';
 export default {
     props: {
-        card: Object
+        card: Object,
     }
 }
 </script>
 
 
 <template>
-    <div>
+
         <div v-for="(card, i) in Cards" :key="1">
-            
-            
+   
         </div>
         
-    </div>
+
 
 </template>
+
+
+
+<style lang="scss" scoped>
+
+@use '../assets/scss/partials/variables' as *;
+
+
+
+</style>
 
 
 
